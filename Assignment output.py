18fc59a782e2fd95f68cf031d7e9dbf4e9b66a9a
Shreyash2003 <104262663+Shreyash2003@users.noreply.github.com>
2022-05-27 21:10:58 +0530

    Add files via upload

---
+++ Assignment output.py
@@ -0,0 +1,33 @@
+C:\Users\shrey\PycharmProjects\pythonProject39\venv\Scripts\python.exe C:/Users/shrey/PycharmProjects/pythonProject39/main.py
+Q1
+Enter mark:82
+Grade: A
+Continue?(y/n)n
+Q2
+Enter year:2016
+Not a leap year
+Q3
+Question 1:4x7=28
+Right!
+Question 2:8x3=20
+Wrong. The correct answer is 24
+Question 3:2x8=16
+Right!
+Question 4:5x6=31
+Wrong. The correct answer is 30
+Question 5:0x6=01
+Wrong. The correct answer is 0
+Question 6:7x2=14
+Right!
+Question 7:3x6=13
+Wrong. The correct answer is 18
+Question 8:8x7=56
+Right!
+Question 9:2x3=6
+Right!
+Question 10:6x3=19
+Wrong. The correct answer is 18
+Q4
+The number of pieces in the bowl are: 177
+
+Process finished with exit code 0
